b4578b977c613b1982fc0439a5436f93b157759c
DevaNurdiyawanto-HackerOne <81873740+DevaNurdiyawanto-HackerOne@users.noreply.github.com>
2021-04-04 01:32:13 +0700

    Delete RootInstallPackage.sh

--- RootInstallPackage.sh
+++
@@ -1,1 +0,0 @@
-apt install git php curl wget nano python python2 nmap ruby vim nodejs electrum build-essential cmake libjpeg-turbo libpng python make yarn perl sudo python-pip python2-pip python3-pip -y
