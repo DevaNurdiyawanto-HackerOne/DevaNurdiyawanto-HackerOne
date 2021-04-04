bcc6fa2f3a7c64882dd2bf317f48031a301459c9
devanurdiyawanto <81873740+DevaNurdiyawanto-HackerOne@users.noreply.github.com>
2021-04-05 02:41:54 +0700

    Update ccminer-Verus.sh

--- ccminer-Verus.sh
+++ ccminer-Verus.sh
@@ -1,5 +1,5 @@
-apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git nano -y
-git clone https://github.com/monkins1010/ccminer.git
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh
 chmod +x configure.sh
