4ce9b76bb713cc1a8fbf2f6da6bcfd36316e5928
devanurdiyawanto <81873740+DevaNurdiyawanto-HackerOne@users.noreply.github.com>
2021-04-04 23:28:07 +0700

    Create sqlscan.sh

---
+++ sqlscan.sh
@@ -0,0 +1,3 @@
+sudo apt install php php-bz2 php-curl php-mbstring curl -y
+sudo curl https://raw.githubusercontent.com/Cvar1984/sqlscan/dev/build/main.phar --output /usr/local/bin/sqlscan
+chmod +x /usr/local/bin/sqlscan
