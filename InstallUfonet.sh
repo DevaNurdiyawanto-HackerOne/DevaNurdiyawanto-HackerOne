cf393435f91e28b9287c55b911cbfc71e5745f06
devanurdiyawanto <81873740+DevaNurdiyawanto-HackerOne@users.noreply.github.com>
2021-04-04 23:44:20 +0700

    Create InstallUfonet.sh

---
+++ InstallUfonet.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/epsylon/ufonet.git
+cd ufonet
+sudo apt-get install python3-pycurl python3-geoip python3-whois python3-crypto python3-requests python3-scapy libgeoip1 libgeoip-dev -y
+python3 setup.py install
