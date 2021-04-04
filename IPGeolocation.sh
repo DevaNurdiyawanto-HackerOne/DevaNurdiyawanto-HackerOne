3662bfa3bea3ec572b542d4c8533e79a20f09e3d
devanurdiyawanto <81873740+DevaNurdiyawanto-HackerOne@users.noreply.github.com>
2021-04-04 23:09:25 +0700

    Update IPGeolocation.sh

--- IPGeolocation.sh
+++ IPGeolocation.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/maldevel/IPGeoLocation
 cd IPGeoLocation
 apt-get install python3-setuptools -y
-pip3 install -r requirements.txt --user
+pip3 install -r requirements.txt
